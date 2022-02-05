371f10b14d20f62e189c25ad21611aa877b9cc61
Kochelaev <qrbox@mail.ru>
2022-02-05 23:45:22 +0400

    add service

---
+++ marketplace/app/Services/Service.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use App\Models\Type;
+
+class Service
+{
+    public function sayHello()
+    {
+        return "hello world!";
+    }
+}
